ffe39a7f9bb1be98bb79a84b271287f93752cf03
Douglas Sellers <douglas.sellers@gmail.com>
2018-01-17 15:45:01 -0800

    Fixed the final file name

--- render_default_images.rb
+++ render_default_images.rb
@@ -12,7 +12,8 @@ def build_image_file_name( base_filename, input_directory, color )
   
 end
 
-def build_combine_files_commands( files, side, length, color, base_filename, search_directory, output_directory )
+def build_combine_files_commands( files, side, length, color, search_directory, output_directory )
+  final_filename = "#{side}-default-#{length}-#{color}.png"
   commands = []
   command = "convert #{build_image_file_name(files['bottom'], search_directory, color)} "
   [files['belt'],files['neckline']].each do |file|
