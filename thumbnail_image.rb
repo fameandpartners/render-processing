eb7ee85a08267d4cce006100292df61b46404ba6
Douglas Sellers <douglas.sellers@gmail.com>
2018-01-18 21:32:45 -0800

    We now print the command set to verify it looks right

--- thumbnail_image.rb
+++ thumbnail_image.rb
@@ -42,12 +42,13 @@ Dir.mkdir("#{output_directory}/top-crop") unless File.exists?("#{output_director
 commands = []
 File.readlines( list_of_files ).each do |file_line|
   filename = "#{base_director}/#{file_line.split( ' ' ).last}"
-  puts filename
   command_set = []
   output_file_name = filename.split( '/' ).last
   command_set << "convert  -trim  \"#{filename}\"  -resize x142 -background transparent -gravity Center -extent 142x142! \"#{output_directory}/142x142/#{output_file_name}\""
   command_set << "convert  -trim \"#{filename}\"  -chop '0x33%' +repage -resize 142x -background transparent -gravity Center -extent 142x142! \"#{output_directory}/bottom-crop/#{output_file_name}\"" 
   command_set << "convert  -trim  \"#{filename}\" -gravity SouthWest -chop '0x33%' +repage -resize 142x -background transparent -gravity Center -extent 142x142! \"#{output_directory}/top-crop/#{output_file_name}\""
+
+  puts command_set
   commands << command_set
 end
 
