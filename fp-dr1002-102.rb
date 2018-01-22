8af6aa2670c519b65ff293e5669bf4aa994e3baf
Douglas Sellers <douglas.sellers@gmail.com>
2018-01-22 11:07:49 -0800

    Added in t9

--- fp-dr1002-102.rb
+++ fp-dr1002-102.rb
@@ -9,7 +9,7 @@ class Dress
   def initialize
     @name = 'Fit & Flare'
     @number_of_colors = 17;
-    @customization_list = %w( t1 t3 t5 t6 t15 t24 t27 t31 t36 t46 t51 t57 t60 t63 b3 b4 b5 b14 a2 a5 ) 
+    @customization_list = %w( t1 t3 t5 t6 t9 t15 t24 t27 t31 t36 t46 t51 t57 t60 t63 b3 b4 b5 b14 a2 a5 ) 
     @starting_json = Hash.new { |hash,key| hash[key] = Hash.new {|hash2,key2| hash2[key2] = Hash.new } }
     @all_lengths = { "micro": "Micro-Mini", "knee": "Knee", "mini": "Mini", "midi": "Midi", "ankle": "Ankle", "maxi": "Maxi"}
     @search_directories = ["/home/ubuntu/Dropbox/renderfiles/Bridesmaid Collection/11. Models and Customisations/Fit and Flare/Rendered PNG's", "/home/ubuntu/Dropbox/renderfiles/Bridesmaid Collection/11. Models and Customisations/The Column Dress and Jump Suit", "/home/ubuntu/Dropbox/renderfiles/Bridesmaid Collection/11. Models and Customisations/Two Piece/Render PNGs"]
