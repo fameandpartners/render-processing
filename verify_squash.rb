9b78d3974e79490fcd228c775bae21b1a91b2cf7
Douglas Sellers <douglas.sellers@gmail.com>
2018-01-24 22:10:53 -0800

    Small typo

--- verify_squash.rb
+++ verify_squash.rb
@@ -6,7 +6,7 @@ csv = ARGV.last
 
 CSV.foreach( csv, :headers => false ) do |row|
   filename = "#{row[0].split( ' ' ).sort.join( '-' )}-#{size}"
-  files = Dir.glob( "#{directory}/#{file_name}*.png" )
+  files = Dir.glob( "#{directory}/#{filename}*.png" )
   puts filename if files.empty?
 end
 
