b425b47afef159917c1410cb0c0ba84662702c59
Douglas Sellers <douglas.sellers@gmail.com>
2018-01-24 22:41:00 -0800

    Made it so that the output is better

--- verify_squash.rb
+++ verify_squash.rb
@@ -7,7 +7,7 @@ csv = ARGV.last
 CSV.foreach( csv, :headers => false ) do |row|
   filename = "#{row[0].split( '-' ).sort.join( '-' )}-#{size}"
   files = Dir.glob( "#{directory}/#{filename}*.png" )
-  puts "Verifying #{directory}/#{filename}*.png (#{files.count}"
+  puts "Verifying #{directory}/#{filename}*.png (#{files.count})"
 end
 
 
