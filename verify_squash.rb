4d638d6855e1c929d5e783ee7b9d169ab7ea7525
Douglas Sellers <douglas.sellers@gmail.com>
2018-01-24 22:25:41 -0800

    Made it so that we sort correctly now

--- verify_squash.rb
+++ verify_squash.rb
@@ -5,7 +5,7 @@ size = ARGV.first
 csv = ARGV.last
 
 CSV.foreach( csv, :headers => false ) do |row|
-  filename = "#{row[0].split( ' ' ).sort.join( '-' )}-#{size}"
+  filename = "#{row[0].split( '-' ).sort.join( '-' )}-#{size}"
   files = Dir.glob( "#{directory}/#{filename}*.png" )
   puts "#{row[0]}=#{filename}"  if files.empty?
 end
