ce6b4c3e9469b59febb990e2336f159c8f2b0fd0
Douglas Sellers <douglas.sellers@gmail.com>
2018-01-24 22:23:41 -0800

    Made it so that now the output is a bit better

--- verify_squash.rb
+++ verify_squash.rb
@@ -7,7 +7,7 @@ csv = ARGV.last
 CSV.foreach( csv, :headers => false ) do |row|
   filename = "#{row[0].split( ' ' ).sort.join( '-' )}-#{size}"
   files = Dir.glob( "#{directory}/#{filename}*.png" )
-  puts filename if files.empty?
+  puts "#{row[0]}=#{filename}"  if files.empty?
 end
 
 
