856d9efa53e5a5c21891cbb3493b166900a8ad7a
Douglas Sellers <douglas.sellers@gmail.com>
2018-01-25 23:00:52 -0800

    removed extra printing

--- verify_colors.rb
+++ verify_colors.rb
@@ -7,11 +7,9 @@ directory = dress.search_directories.first
 ['Front', 'Back' ].each do |subdirectory|
   file_list = []
 
-  puts "#{directory}/#{subdirectory}/*.png"
   files = Dir.glob("#{directory}/#{subdirectory}/*.png")
 
   files.each do |file|
-    puts file
     split_file = file.split( '/' ).last.split( '.' ).first.split( '_' )
     split_file.pop
     file_list << split_file.join( '_' )
@@ -20,7 +18,6 @@ directory = dress.search_directories.first
   file_list = file_list.uniq
 
   file_list.each do |part_of_file|
-    puts "Verifying #{part_of_file}"
     puts "Missing colors for #{part_of_file}" unless Dir.glob("#{directory}/#{subdirectory}/#{part_of_file}_00*.png").count == (dress.number_of_colors + 1)
   end
 end
