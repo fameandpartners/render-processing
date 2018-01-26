2cee2ba31e16b81089addaa3d357124e5c202bc6
Douglas Sellers <douglas.sellers@gmail.com>
2018-01-26 11:17:28 -0800

    Made verify work a lot better

--- verify_final_dress.rb
+++ verify_final_dress.rb
@@ -1,6 +1,22 @@
 dress_id = ARGV.first
+require "./#{dress_id}"
 
-results = `aws s3 ls s3://mkt-fameandpartners/renders/composites/fp-dr1005-102/800x800`
+def verify_results( results, image_size )
+  dress.all_lengths.keys.each do |length|
+    length_count = (results.select{ |file| !file.index( length.to_s ).nil? }).count
+    if( length_count < 10000 )
+      puts "#{length} is missing items for image size #{image_size}" 
+    end
+  end
+end
 
-results = results.split("\n")
-puts results.count
+dress = Dress.new
+
+
+["800x800", "142x142", "bottom-crop", "top-crop"].each do |image_size|
+  puts "Verify #{image_size}"
+  results = `aws s3 ls s3://mkt-fameandpartners/renders/composites/#{dress_id.downcase}/#{image_size} --recursive`
+  results = results.split("\n")
+  verify_results( results, image_size )
+end
+
