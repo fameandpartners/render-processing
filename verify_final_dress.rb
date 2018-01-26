b4df5247f75204036f7066d396c25fa05c3483b6
Douglas Sellers <douglas.sellers@gmail.com>
2018-01-26 11:21:09 -0800

    Made it so we get some diagnostic info out of the verifier

--- verify_final_dress.rb
+++ verify_final_dress.rb
@@ -1,7 +1,7 @@
 dress_id = ARGV.first
 require "./#{dress_id}"
 
-def verify_results( results, image_size )
+def verify_results( dress, results, image_size )
   dress.all_lengths.keys.each do |length|
     length_count = (results.select{ |file| !file.index( length.to_s ).nil? }).count
     if( length_count < 10000 )
@@ -17,6 +17,7 @@ dress = Dress.new
   puts "Verify #{image_size}"
   results = `aws s3 ls s3://mkt-fameandpartners/renders/composites/#{dress_id.downcase}/#{image_size} --recursive`
   results = results.split("\n")
-  verify_results( results, image_size )
+  puts "Total results: #{results.length}'
+  verify_results( dress,results, image_size )
 end
 
