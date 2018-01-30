5ed78698422e5dc0393dc824345fe9a0f45a53df
Douglas Sellers <douglas.sellers@gmail.com>
2018-01-30 15:37:32 -0800

    Made it so that now we have ignored customizations

--- fp-dr1006-102.rb
+++ fp-dr1006-102.rb
@@ -11,7 +11,7 @@ class Dress
     @name = 'Slip Dress'
     @number_of_colors = 17;
     @customization_list = %w( t6 t9 t10 t11 t16 t18 t19 t25 t28 t45 t49 t57 b4 b14 b17 b18 a2 a5 )
-    @ignorable_customizations = %w()
+    @ignorable_customizations = %w( b17 b18 )
     @starting_json = Hash.new { |hash,key| hash[key] = Hash.new {|hash2,key2| hash2[key2] = Hash.new } }
     @all_lengths = { "micro": "Micro-Mini", "knee": "Knee", "mini": "Mini", "midi": "Midi", "ankle": "Ankle", "maxi": "Maxi"}
     @search_directories = ["/home/ubuntu/Dropbox/renderfiles/Bridesmaid Collection/11. Models and Customisations/Slip Dress/Rendererd PNG's/Sides"]
