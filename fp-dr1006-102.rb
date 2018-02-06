b6e8ab72f853fddafafbdc99e39e37d56d2b0059
Douglas Sellers <douglas.sellers@gmail.com>
2018-02-06 14:24:14 -0800

    Made it so that now the bottom layer on the defaults is gone for slip

--- fp-dr1006-102.rb
+++ fp-dr1006-102.rb
@@ -17,43 +17,37 @@ class Dress
     @search_directories = ["/home/ubuntu/Dropbox/renderfiles/Bridesmaid Collection/11. Models and Customisations/Slip Dress/Rendererd PNG's/Sides"]
     @starting_json['Micro-Mini']['default'] = { "front":
                                                   {
-                                                    "bottom": "base_micro_mini_bottom_front",
                                                     "belt": "base_micro_mini_belt_front",
                                                     "neckline": "base_neckline_front"
                                                   },
                                                 "back":
                                                   {
                                                     "neckline": "base_neckline_back",
-                                                    "belt": "base_micro_mini_belt_back",
-                                                    "bottom": "base_micro_mini_bottom_back"
+                                                    "belt": "base_micro_mini_belt_back"
                                                   }
                                               }
     
     @starting_json['Mini']['default'] = { "front":
                                             {
-                                              "bottom": "base_mini_bottom_front",
                                               "belt": "base_mini_belt_front",
                                               "neckline": "base_neckline_front"
                                             },
                                           "back":
                                             {
                                              "neckline": "base_neckline_back",
-                                             "belt": "base_mini_belt_back",
-                                             "bottom": "base_midi_bottom_back"
+                                             "belt": "base_mini_belt_back"
                                             }
                                         }
     
     @starting_json['Knee']['default'] = { "front":
                                             {
-                                             "bottom": "base_knee_bottom_front",
                                              "belt": "base_knee_belt_front",
                                              "neckline": "base_neckline_front"
                                             },
                                           "back":
                                             {
                                              "neckline": "base_neckline_back",
-                                             "belt": "base_knee_belt_back",                                             
-                                             "bottom": "base_knee_bottom_back"
+                                             "belt": "base_knee_belt_back"
                                             }
                                         }
 
@@ -61,43 +55,37 @@ class Dress
 
     @starting_json['Midi']['default'] = { "front":
                                             {
-                                              "bottom": "base_midi_bottom_front",
                                              "belt": "base_midi_belt_front",                                             
                                               "neckline": "base_neckline_front"
                                             },
                                           "back":
                                             {
                                               "neckline": "base_neckline_back",
-                                             "belt": "base_midi_belt_back",
-                                              "bottom": "base_midi_bottom_back"
+                                             "belt": "base_midi_belt_back"
                                             }
                                         }
     
     @starting_json['Ankle']['default'] = { "front":
                                              {
-                                               "bottom": "base_ankle_bottom_front",
                                              "belt": "base_ankle_belt_front", 
                                               "neckline": "base_neckline_front"
                                              },
                                            "back":
                                              {
                                                "neckline": "base_neckline_back",
-                                              "belt": "base_ankle_belt_back",                                              
-                                              "bottom": "base_ankle_bottom_back"
+                                              "belt": "base_ankle_belt_back"
                                              }
                                          }
     
     @starting_json['Maxi']['default'] = { "front":
                                             {
-                                              "bottom": "base_maxi_bottom_front",
                                             "belt": "base_maxi_belt_front",                                              
                                               "neckline": "base_neckline_front"
                                              },
                                            "back":
                                              {
                                                "neckline": "base_neckline_back",
-                                              "belt": "base_maxi_belt_back",
-                                               "bottom": "base_maxi_bottom_back"
+                                              "belt": "base_maxi_belt_back"
                                                   
                                              }
                                         }
